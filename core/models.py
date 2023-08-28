12622b9f8555c20fb9cf2c5b880da5dd7570c603
Arisbek <71332684+Arisbek@users.noreply.github.com>
2023-08-28 13:19:23 +0600

    ''

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class transport(models.Model):
+    mark = models.CharField(max_length=20)
+    number = models.CharField(unique=True, max_length=20)
+    trailer = models.CharField(max_length=20, blank=True)
+    class Meta:
+        verbose_name = "Транспортное средство"
+        verbose_name_plural = "Транспортные средства"
