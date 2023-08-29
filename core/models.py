3ad5da0f153e765ca64445bbacacce06eab303fd
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-08-28 21:25:17 -0700

    id

--- core/models.py
+++ core/models.py
@@ -1,24 +1,11 @@
 from django.db import models
 
-class Name(models.Model):
+class GSM(models.Model):
     name = models.CharField('Наименование', max_length=80, null=True, blank=False)
     id = models.PositiveIntegerField('id', primary_key=True)
     class Meta:
         verbose_name = 'Наименование:'
         verbose_name_plural = 'Наименование:'
-# Create your models here.
-
-class ReportDate(models.Model):
-    start_date = models.DateTimeField(auto_now_add=True, blank=False)
-    end_date = models.DateTimeField(auto_now_add=True, blank=False)
-
-    class Meta:
-        verbose_name = 'star_date'
-        verbose_name_plural = 'end_date'
 
 
-class DriversName(models.Model):
-    full_name = models.CharField(blank=False, max_length=55)
 
-    class Meta:
-        verbose_name = 'Ф.И.О'
