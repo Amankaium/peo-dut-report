3e946728e5ee130f144781ac360e8b47fb9464fa
Amankaium <kaium@list.ru>
2023-08-29 11:40:05 +0600

    added trasport list api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Transport
+from .serializers import TransportSerializer
 
-# Create your views here.
+
+class TransportAPIView(APIView):
+    def get(self, request):
+        transports = Transport.objects.all()
+        serializer = TransportSerializer(
+            instance=transports,
+            many=True
+        )
+        data = serializer.data
+        return Response(data)
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import TransportAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('transport/', TransportAPIView.as_view(), name='transport')
 ]
