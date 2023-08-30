c0f30b9d8f892c8a3d168a3e74c939ee7eb3478a
azamaidana <141002326+azamaidana@users.noreply.github.com>
2023-08-30 11:59:39 +0600

    add model card, fixed

--- core/migrations/0010_rename_map_card.py
+++ core/migrations/0010_rename_map_card.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0005_map'),
+        ('core', '0009_map'),
     ]
 
     operations = [
