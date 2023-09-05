56e7d184984dec9e1d10cef9159707d21e1d7fbe
Amankaium <kaium@list.ru>
2023-09-05 12:22:06 +0600

    added viewset

--- core/views/report.py
+++ core/views/report.py
@@ -2,8 +2,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework import generics
-from ..models import *
-from ..serializers import *
+from core.models import *
+from core.serializers import *
+
+
+class ReportAPIVew(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+
+
+class ReportUpdateAPIVew(APIView):
+    def put(self, request, *args, **kwargs):
+        report_object = Report.objects.get(pk=kwargs["pk"])
+        serializer = ReportSerializer(instance=report_object, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, 202)
+        return Response(serializer.errors, 400)
+
+
+class ReportDeleteAPIVew(APIView):
+    def delete(self, request, *args, **kwargs):
+        report_object = Report.objects.get(pk=kwargs["pk"])
+        report_object.delete()
+        return Response("Successfully deleted", 204)
 
 class ReportDetailAPIView(APIView):
     def get(self, request, *args, **kwargs):
@@ -17,6 +39,7 @@ class ReportCreateAPIVew(CreateAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
 
+
 class ReportListAPIVew(ListAPIView):
     def get(self, request):
         reports = Report.objects.all()
@@ -26,3 +49,4 @@ class ReportListAPIVew(ListAPIView):
         )
         data = serializer.data
         return Response(data)
+
