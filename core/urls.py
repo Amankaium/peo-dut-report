ce6441a39803fbb2076bca0f9dfbb5e2baff9310
JohnnyEvs <sj1807@mail.ru>
2023-09-11 15:15:56 +0900

    added html fuel types

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', GetDataView.as_view(), name='home'),
     path('get-transports/', GetTransportView.as_view(), name='get-transports'),
-]
+    path('get_fuel_type/', GetFuelTypeView.as_view(), name='get_fuel_type'),
+    ]
